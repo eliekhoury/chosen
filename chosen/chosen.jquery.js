de7ccf92ad1c2a603b2ce8136ab84c7a0bacb99e
eliekhoury <elie@ekhoury.com>
2012-06-07 10:05:51 -0700

    Show/Hide drop down rather than moving it to the left. Removing the width style from the search and dropdown (using box-style and right in the css).

--- chosen/chosen.jquery.js
+++ chosen/chosen.jquery.js
@@ -323,18 +323,17 @@ Copyright (c) 2011 by Harvest
         style: 'width: ' + this.f_width + 'px;'
       });
       if (this.is_multiple) {
-        container_div.html('<ul class="chzn-choices"><li class="search-field"><input type="text" value="' + this.default_text + '" class="default" autocomplete="off" style="width:25px;" /></li></ul><div class="chzn-drop" style="left:-9000px;"><ul class="chzn-results"></ul></div>');
+        container_div.html('<ul class="chzn-choices"><li class="search-field"><input type="text" value="' + this.default_text + '" class="default" autocomplete="off" style="width:25px;" /></li></ul><div class="chzn-drop" style="display: none;"><ul class="chzn-results"></ul></div>');
       } else {
-        container_div.html('<a href="javascript:void(0)" class="chzn-single chzn-default"><span>' + this.default_text + '</span><div><b></b></div></a><div class="chzn-drop" style="left:-9000px;"><div class="chzn-search"><input type="text" autocomplete="off" /></div><ul class="chzn-results"></ul></div>');
+        container_div.html('<a href="javascript:void(0)" class="chzn-single chzn-default"><span>' + this.default_text + '</span><div><b></b></div></a><div class="chzn-drop" style="display: none;"><div class="chzn-search"><input type="text" autocomplete="off" /></div><ul class="chzn-results"></ul></div>');
       }
       this.form_field_jq.hide().after(container_div);
-      this.container = $('#' + this.container_id);
+      this.container = container_div;
       this.container.addClass("chzn-container-" + (this.is_multiple ? "multi" : "single"));
       this.dropdown = this.container.find('div.chzn-drop').first();
       dd_top = this.container.height();
       dd_width = this.f_width - get_side_border_padding(this.dropdown);
       this.dropdown.css({
-        "width": dd_width + "px",
         "top": dd_top + "px"
       });
       this.search_field = this.container.find('input').first();
@@ -348,9 +347,6 @@ Copyright (c) 2011 by Harvest
         this.search_container = this.container.find('div.chzn-search').first();
         this.selected_item = this.container.find('.chzn-single').first();
         sf_width = dd_width - get_side_border_padding(this.search_container) - get_side_border_padding(this.search_field);
-        this.search_field.css({
-          "width": sf_width + "px"
-        });
       }
       this.results_build();
       this.set_tab_index();
@@ -584,9 +580,8 @@ Copyright (c) 2011 by Harvest
         chosen: this
       });
       this.dropdown.css({
-        "top": dd_top + "px",
-        "left": 0
-      });
+        "top": dd_top + "px"
+      }).show();
       this.results_showing = true;
       this.search_field.focus();
       this.search_field.val(this.search_field.val());
@@ -601,9 +596,7 @@ Copyright (c) 2011 by Harvest
       this.form_field_jq.trigger("liszt:hiding_dropdown", {
         chosen: this
       });
-      this.dropdown.css({
-        "left": "-9000px"
-      });
+      this.dropdown.hide();
       return this.results_showing = false;
     };
 
